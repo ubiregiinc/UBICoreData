b225063a80ff0e2a24d333174bdc2c68fcf008f2
Nobuhito Muto <nobuhito.muto@ubiregi.com>
2024-03-05 17:04:22 +0900

    [fix] PrivacyInfo.xcprivacyの追加

--- Package.swift
+++ Package.swift
@@ -20,6 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UBICoreData",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/UBICoreData",
+            resources: [
+                .process("PrivacyInfo.xcprivacy")
+            ]),
     ]
 )
--- UBICoreData.podspec
+++ UBICoreData.podspec
@@ -8,6 +8,6 @@ Pod::Spec.new do |s|
   s.platform     = :ios, '12.0'
   s.requires_arc = true
   s.source       = { :git => 'https://github.com/ubiregiinc/UBICoreData.git', :tag => s.version.to_s }
-  s.source_files = 'Sources/UBICoreData/*'
+  s.source_files = 'Sources/UBICoreData/*.{h,m,xcprivacy}'
   s.framework    = 'CoreData'
 end
